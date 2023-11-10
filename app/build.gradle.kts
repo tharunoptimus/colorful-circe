b1efb33c90e81d0230d47b1577599e3fd7aa54eb
IN45704543 <Tharunraj.Mailvakanan@tesco.com>
2023-11-10 12:15:51 +0530

    Changed compilesdk version to 34.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.colorfulcircle"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.colorfulcircle"
@@ -32,6 +32,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
     }
 }
 
