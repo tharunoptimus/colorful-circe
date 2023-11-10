2e50522e16ffde893f8df906964430e4ac3c37e0
IN45704543 <Tharunraj.Mailvakanan@tesco.com>
2023-11-10 12:16:24 +0530

    Added binding and the function to generate random colors

--- app/src/main/java/com/example/colorfulcircle/MainActivity.kt
+++ app/src/main/java/com/example/colorfulcircle/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.colorfulcircle
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.colorfulcircle.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private fun generateRandomColorDrawable(): GradientDrawable {
+        val random = Random
+        val color = String.format("#%06x", random.nextInt(0xffffff + 1))
+        val drawable = GradientDrawable()
+        drawable.shape = GradientDrawable.OVAL
+        drawable.setColor(Color.parseColor(color))
+        return drawable
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.apply {
+            circle.setOnClickListener {
+                circle.background = generateRandomColorDrawable()
+            }
+        }
+
+        setContentView(binding.root)
+
     }
 }
